b9d240fa34dad87fb047967dc35daacdd307f369
anic17 <58483910+anic17@users.noreply.github.com>
2020-12-28 19:55:54 +0100

    Updated FINDSTR bug

--- BatError.bat
+++ BatError.bat
@@ -5,5 +5,5 @@ if not exist "%~dp0baterror.msg" (
 	echo;The system cannot find the file specified: baterror.msg
 	exit /b 2
 )
-for /f "eol=# tokens=2 delims==" %%A in ('findstr /bc:"%errno%" baterror.msg') do echo;%%A
+for /f "eol=# tokens=2 delims==" %%A in ('findstr /bc:"%errno%" "%~dp0baterror.msg"') do echo;%%A
 exit /b %errno%
