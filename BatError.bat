a9f475e4461fd2abd2734628f0d3d1f37becd3da
anic17 <58483910+anic17@users.noreply.github.com>
2020-12-28 19:38:09 +0100

    BatError main file

---
+++ BatError.bat
@@ -0,0 +1,9 @@
+@echo off
+set errno=%errorlevel%
+
+if not exist "%~dp0baterror.msg" (
+	echo;The system cannot find the file specified: baterror.msg
+	exit /b 2
+)
+for /f "eol=# tokens=2 delims==" %%A in ('findstr /bc:"%errno%" baterror.msg') do echo;%%A
+exit /b %errno%
